03363c191ab31fbd1276a0138aed9e18c5ec6c12
VLAD-KONATA <mfyang2000@qq.com>
2025-06-03 16:35:12 +0800

    update

---
+++ main_dist.py
@@ -0,0 +1,225 @@
+import config
+args, unparsed = config.get_args()
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import random
+import numpy as np
+from tqdm import tqdm
+
+from data import trainSet
+from util_evaluation import calc_psnr,calc_ssim
+import datetime
+from select_model import select_model
+import optim
+#import val
+from select_loss import Select_Loss
+import torch.nn.functional as F
+from losses.laplacianpyramid import *
+
+from opt import *
+import torch.nn as nn
+import json
+from importlib import import_module
+
+def args_add_additinoal_attr(args,json_path):
+    dic = json.load(open(json_path,'r',))
+    for key,value in dic.items():
+        if key == '//':
+            continue
+        setattr(args,key,value)
+
+def select_tmodel(args):
+    opt_path = f'opt/{args.model}_multi.json'
+    args_add_additinoal_attr(args, opt_path)
+    module = import_module(f'model_zoo.{args.model.lower()}.basic_model')
+    model = module.make_model(args)
+    return model
+####################################################################
+# seed
+GLOBAL_SEED = 777
+random.seed(GLOBAL_SEED)
+np.random.seed(GLOBAL_SEED)
+torch.manual_seed(GLOBAL_SEED)
+torch.cuda.manual_seed(GLOBAL_SEED)
+torch.cuda.manual_seed_all(GLOBAL_SEED)
+student=False
+
+args.ckpt_dir = 'experiments/'+args.model+'/'+args.ckpt_dir
+os.makedirs(args.ckpt_dir,exist_ok=True)
+
+if len(args.gpu_id) > 1:
+    args.parallel = True
+
+# data
+trainset = trainSet(data_root=args.traindata_path,args=args)
+# batch_size = args.batch_size*len(device_ids)
+dataloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,\
+                shuffle=False,num_workers=args.num_workers, pin_memory=False)
+if student:
+    smodel = select_model(args)
+    # model
+    tmodel = select_tmodel(args)
+    checkpoint = torch.load('/home/konata/Git/mnet/experiments/mamba_unet_multi/IXI_x2_2x4b/pth/0500.pth', map_location=torch.device('cpu'))
+    tmodel.load_state_dict(checkpoint['state_dict'])
+    smodel = smodel.cuda()
+else:
+    tmodel=select_model(args)
+
+
+
+
+tmodel = tmodel.cuda()
+
+#### optim ####
+optimizer = torch.optim.Adam(smodel.parameters(), lr=args.lr)
+# optimizer = torch.optim.Adam(model.parameters(),lr=args.lr, betas=(args.beta1, args.beta2), eps=args.eps)
+scheduler = optim.select_scheduler(args,optimizer)
+
+
+    # 定义优化器
+#### loss ####
+hard_loss = nn.L1Loss()
+soft_loss = nn.KLDivLoss(reduction="batchmean")
+#soft_loss = LaplacianLoss()
+loss_function = Select_Loss(args).cuda()
+
+########################### train ###################################
+# log
+with open(args.ckpt_dir + '/logs.txt',mode='a+') as f:
+    s = "START EXPERIMENT\n"
+    f.write(s)
+    for i,j in args.__dict__.items():
+        f.write(str(i)+' : '+str(j)+'\n')
+
+# amp
+if args.amp:
+    scaler = torch.cuda.amp.GradScaler()
+    autocast = torch.cuda.amp.autocast
+
+temp=7
+alpha=0.3
+
+smodel.train()
+for epoch in tqdm(range(args.start_epoch,args.max_epoch)):
+    loss_epoch = 0
+    psnr_epoch = 0
+
+    for iter, hr in tqdm(enumerate(dataloader)):
+                 
+        if len(hr.shape) == 5:
+            gt = torch.cat([i for i in hr],0) #[bz,h,w,7]
+        else: gt = hr
+        lr = gt[...,::args.upscale] # [bz,h,w,4]
+        if torch.cuda.is_available():
+            gt = Variable(gt.cuda())
+            lr = Variable(lr.cuda())
+        
+        optimizer.zero_grad()
+        if student:
+            if args.amp:
+                with autocast():
+                    with torch.no_grad():
+                        tsr,tmiddle = tmodel(lr,False)
+                    #print('pred_tmodel')
+                    # 学生模型预测
+                    ssr,smiddle = smodel(lr,True)
+                    #print('pred_smodel')
+
+                    # 计算hard_loss
+                    student_hard_loss = hard_loss(ssr,gt)
+
+                    # chatgpt版Loss
+                    soft_student_outputs = F.log_softmax(ssr / temp, dim=1)
+                    soft_teacher_outputs = F.softmax(tsr/temp,dim=1)
+
+                    ditillation_loss = soft_loss(soft_student_outputs,soft_teacher_outputs)
+                    loss = alpha * student_hard_loss + (1-alpha) * temp * temp * ditillation_loss
+
+                    scaler.scale(loss).backward()
+                    scaler.step(optimizer)
+                    scaler.update()
+            else:
+                sr = smodel(lr)
+                '''
+                loss_iter = loss_function(sr,gt)
+                loss_iter.backward()
+                '''
+                optimizer.step()
+        else:
+            if args.amp:
+                with autocast():
+                    with torch.no_grad():
+                        tsr,tmiddle = tmodel(lr,False)
+                    loss = loss_function(sr,gt)
+                    scaler.scale(loss).backward()
+                    scaler.step(optimizer)
+                    scaler.update()
+            else:
+                sr = smodel(lr)
+                '''
+                loss_iter = loss_function(sr,gt)
+                loss_iter.backward()
+                '''
+                optimizer.step()
+
+        psnr_iter = 0
+        for bz in range(gt.shape[0]):
+            psnr_iter += calc_psnr(gt[bz, :, :, :],ssr[bz, :, :, :]).item()
+        psnr_iter /= (bz+1)  
+
+        #### log ####    
+        lr_tmp = optimizer.state_dict()['param_groups'][0]['lr']
+        log = r"epoch[{}/{}] iter[{}/{}] psnrTr:{:.6f} lossTr:{:.12f} lr:{:.12f}"\
+            .format(epoch+1, args.max_epoch , \
+                iter+1, len(dataloader),\
+                psnr_iter,loss,lr_tmp)
+        now = str(datetime.datetime.now())
+        print(now+' '+log)
+
+        loss_epoch += loss
+        psnr_epoch += psnr_iter
+
+    loss_epoch /= (iter+1)
+    psnr_epoch /= (iter+1)
+
+    #### lr schedule ####
+    if args.schedule == 'step':
+        scheduler.step()
+    elif args.schedule == 'cos_lr':
+        #### torch.cos_lr ####
+        # scheduler.step()
+        #### timm.cos_lr ####
+        scheduler.step_update(epoch)
+    elif args.schedule == 'Tmin':
+        scheduler.step(loss_epoch)
+    elif args.schedule == 'Tmax':
+        scheduler.step(psnr_epoch)
+
+    epoch +=1
+
+
+    log = r"epoch[{}/{}] psnrTr:{:.6f} lossTr:{:.12f} lr:{:.12f}"\
+    .format(epoch, args.max_epoch , \
+        psnr_epoch,loss_epoch,lr_tmp)
+    now = str(datetime.datetime.now())
+    print(now+' '+log)
+    with open(args.ckpt_dir + '/logs.txt',mode='a+') as f:
+        f.write('\n'+now+log)
+
+    if epoch >int(0.99*args.max_epoch):
+        os.makedirs(args.ckpt_dir+'/pth',exist_ok=True)
+        os.makedirs(args.ckpt_dir+'/keys',exist_ok=True)
+        try:
+            torch.save({'epoch': epoch, 'state_dict': smodel.module.state_dict()}, args.ckpt_dir + '/pth/' + str(epoch).zfill(4) + '.pth')
+        except:
+            torch.save({'epoch': epoch, 'state_dict': smodel.state_dict()}, args.ckpt_dir + '/pth/' + str(epoch).zfill(4) + '.pth')
+
+
+# val_opt = True
+# if val_opt:
+#     val.val(args=args,model=model)
+
---
+++ model_zoo/mamba_unet_dist/basic_model.py
@@ -0,0 +1,233 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import einops
+from functools import partial
+import torch.nn.functional as F
+from einops.layers.torch import Rearrange
+from mamba_ssm import Mamba
+#from unet import UNet
+from .unet import UNet
+
+
+def make_model(args):
+    return newmodel(args)
+
+#####################################################################
+def default_conv(in_channelss, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(
+        in_channelss, out_channels, kernel_size,
+        padding=(kernel_size // 2), bias=bias)
+
+
+class I2Block(nn.Module):
+    def __init__(
+        self, conv, n_feat, kernel_size,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1,head_num=1,win_num_sqrt=16,window_size=16):
+        super(I2Block, self).__init__()
+        inter_slice_branch = [
+            nn.PixelUnshuffle(2),
+            nn.Conv2d(4*n_feat,4*n_feat,3,1,1),
+            nn.ReLU(),
+            nn.Conv2d(4*n_feat,4*n_feat,3,1,1), # +
+            nn.PixelShuffle(2), # +
+            nn.Conv2d(n_feat,n_feat,1,1,0)
+        ]
+        self.inter_slice_branch = nn.Sequential(*inter_slice_branch)
+        self.res_scale = res_scale
+        self.unet=UNet(64,64)
+        self.mamba= Mamba(
+    # This module uses roughly 3 * expand * d_model^2 parameters
+    d_model=256, # Model dimension d_model
+    d_state=16,  # SSM state expansion factor
+    d_conv=4,    # Local convolution width
+    expand=2,    # Block expansion factor
+    )
+
+    def forward(self, x):
+        x_u=self.unet(x)
+       # x_inter = self.inter_slice_branch(x).mul(self.res_scale)
+        mamba_x=einops.rearrange(x,'b d h w -> (b d) h w')
+        mamba_x=mamba_x.contiguous()
+        output = self.mamba(mamba_x)
+        x_mamba=einops.rearrange( output,'(b d) h w -> b d h w',b=x.shape[0])
+        #out = x_inter + x_mamba + x
+        out = x_u + x_mamba + x
+        return out
+
+class I2Group(nn.Module):
+    def __init__(
+        self, conv, n_depth, n_feat, kernel_size,skip_connect=False,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1,head_num=1,win_num_sqrt=16,window_size=16):
+        super().__init__()
+
+        body = [I2Block(conv, n_feat, kernel_size,
+                            bias, bn, act, res_scale,head_num,win_num_sqrt) for _ in range(n_depth)]
+
+        self.body = nn.Sequential(*body)
+    def forward(self,x):
+        x_f = self.body(x)
+        out = x_f
+        return out
+
+
+class CrossViewBlock(nn.Module):
+    def __init__(self,n_feat):
+        super().__init__()
+
+        self.norm = nn.LayerNorm(n_feat)
+
+        self.conv_sag = nn.Sequential(
+            nn.Conv2d(n_feat,n_feat,1,1,0),
+            Rearrange('b c h w -> b h c w'),
+            nn.PixelShuffle(2),
+            nn.Conv2d(64,64,3,1,1),
+            nn.ReLU(),
+            nn.Conv2d(64,64,3,1,1),
+            nn.PixelUnshuffle(2),
+            Rearrange('b h c w -> b c h w'),
+        )
+        
+        self.conv_cor = nn.Sequential(
+            nn.Conv2d(n_feat,n_feat,1,1,0),
+            Rearrange('b c h w -> b w c h'),
+            nn.PixelShuffle(2),
+            nn.Conv2d(64,64,3,1,1),
+            nn.ReLU(),
+            nn.Conv2d(64,64,3,1,1),
+            nn.PixelUnshuffle(2),
+            Rearrange('b w c h -> b c h w'),
+        )
+
+    def forward(self,x):
+        B,C,H,W = x.shape
+        x = einops.rearrange(x,'b c h w -> b (h w) c')
+        x = self.norm(x)
+        x = einops.rearrange(x,'b (h w) c -> b c h w',h=H,w=W)
+
+        x_sag_f = self.conv_sag(x) # b c h w
+        x_cor_f = self.conv_cor(x) # b c h w
+        x_out = x_cor_f + x_sag_f
+        return x_out
+    
+
+class newmodel(nn.Module):
+    def __init__(self,args=None,conv=default_conv,student=True):
+        super(newmodel, self).__init__()
+        self.args = args
+        self.student=student
+        if self.student:
+            blocks=1
+            cblayers=2
+            depth=1
+        else:
+            blocks=4
+            cblayers=3
+            depth=2
+            
+        n_feats = args.n_feats #64
+        kernel_size = args.kernel_size # 3
+        num_blocks = args.num_blocks # 16
+        act = nn.ReLU(True)
+        res_scale = args.res_scale # 1
+        in_slice = args.lr_slice_patch*1
+        out_slice = args.hr_slice_patch
+
+        head_num = args.head_num
+        win_num_sqrt = args.win_num_sqrt
+        window_size = args.window_size
+        self.head = nn.Sequential(conv(in_slice,n_feats,kernel_size),
+                                  nn.ReLU(),
+                                  conv(n_feats,n_feats,kernel_size))
+        
+        modules_body = [
+            I2Group(
+                conv, n_depth=depth,n_feat=n_feats, kernel_size=kernel_size, act=act, res_scale=res_scale, 
+                head_num=head_num, win_num_sqrt=win_num_sqrt,window_size=window_size) for _ in range(blocks)]
+        self.body = nn.ModuleList(modules_body)
+        
+        self.alignment = nn.ModuleList([CrossViewBlock(n_feats) for _ in range(cblayers)])
+
+        self.fuse_align = nn.Conv2d(cblayers*n_feats,n_feats,1,1,0)
+
+        modules_tail = [
+            conv(n_feats, n_feats, kernel_size),
+            nn.ReLU(),
+            conv(n_feats,out_slice,kernel_size)]
+        self.tail = nn.Sequential(*modules_tail)
+        
+        self.aux_s = nn.Sequential(
+                nn.Conv2d(64, 32, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
+                nn.Dropout2d(p=0.1),
+                nn.Conv2d(32, out_slice, kernel_size=1)
+            )
+        
+    def forward(self, x,student=True):
+        #print('model_name=mamba_unet_multi')
+        x = x.permute(0,3,1,2)
+        x = x.contiguous()
+        x_head = self.head(x) 
+        
+        res = x_head
+
+        align_list = []
+        res = self.alignment[0](res)+res
+        align_list.append(res)
+        id_list=[]
+        if student:
+            id_list=[0]
+        else:
+            id_list=[1,3]
+        for id,layer in enumerate(self.body):
+            res = layer(res)
+            if not(student) and id==1:
+                res_middle=self.aux_s(res)
+            if student and id==0:
+                res_middle=self.aux_s(res)
+            if id in id_list:
+                res = self.alignment[id//2+1](res) + res
+                align_list.append(res)
+
+        res = self.fuse_align(torch.cat(align_list,1))
+        
+        res += x_head       
+        
+        out = self.tail(res) # [bz,s,h,w]
+        
+        out[:,::self.args.upscale] = x
+        out = out.permute(0,2,3,1).contiguous()
+        
+        return out,res_middle
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+    args = argparse.Namespace()
+    args.upscale = 2
+    args.n_feats = 64
+    args.kernel_size = 3
+    args.res_scale = 1
+    args.num_blocks = 16
+    args.lr_slice_patch = 4
+    args.hr_slice_patch = (args.lr_slice_patch-1)*args.upscale + 1
+    args.head_num = 1
+    args.win_num_sqrt = 16
+    args.window_size = 16
+    args.n_size = 256
+
+    gpy_id = 0
+    model = newmodel(args).cuda(gpy_id)
+    #x = torch.ones(1,args.n_size,args.n_size,args.lr_slice_patch).cuda(gpy_id)
+    #y = torch.ones(1,args.n_size,args.n_size,args.hr_slice_patch).cuda(gpy_id)
+    x=torch.randn(1,256,256,4).cuda(gpy_id)
+    from torchsummary import summary
+    from thop import profile
+    #summary(model,(256, 256, 4))
+    #pred=model(x)
+    #print(pred.shape)
+    flops,params=profile(model,(x,))
+    print('flops: %.2f M, params: %.2f M' % (flops / 1000000.0, params / 1000000.0))
