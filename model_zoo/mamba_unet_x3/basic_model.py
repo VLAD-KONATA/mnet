eb662c220752e9b5af9b4937453d3d73dbddebec
VLAD-KONATA <mfyang2000@qq.com>
2025-06-30 22:34:28 +0800

    update

---
+++ model_zoo/mamba_unet_x3/basic_model.py
@@ -0,0 +1,413 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import einops
+from functools import partial
+import torch.nn.functional as F
+from einops.layers.torch import Rearrange
+from mamba_ssm import Mamba
+#from .vmambanew import SS2D
+#from .lib_mamba.vmambanew import SS2D
+#from lib_mamba.vmambanew import SS2D
+import pywt
+#from unet import UNet
+from .unet import UNet
+
+def make_model(args):
+    return newmodel(args)
+
+#####################################################################
+def default_conv(in_channelss, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(
+        in_channelss, out_channels, kernel_size,
+        padding=(kernel_size // 2), bias=bias)
+    
+def create_wavelet_filter(wave, in_size, out_size, type=torch.float):
+    w = pywt.Wavelet(wave)
+    dec_hi = torch.tensor(w.dec_hi[::-1], dtype=type)
+    dec_lo = torch.tensor(w.dec_lo[::-1], dtype=type)
+    dec_filters = torch.stack([dec_lo.unsqueeze(0) * dec_lo.unsqueeze(1),
+                               dec_lo.unsqueeze(0) * dec_hi.unsqueeze(1),
+                               dec_hi.unsqueeze(0) * dec_lo.unsqueeze(1),
+                               dec_hi.unsqueeze(0) * dec_hi.unsqueeze(1)], dim=0)
+
+    dec_filters = dec_filters[:, None].repeat(in_size, 1, 1, 1)
+
+    rec_hi = torch.tensor(w.rec_hi[::-1], dtype=type).flip(dims=[0])
+    rec_lo = torch.tensor(w.rec_lo[::-1], dtype=type).flip(dims=[0])
+    rec_filters = torch.stack([rec_lo.unsqueeze(0) * rec_lo.unsqueeze(1),
+                               rec_lo.unsqueeze(0) * rec_hi.unsqueeze(1),
+                               rec_hi.unsqueeze(0) * rec_lo.unsqueeze(1),
+                               rec_hi.unsqueeze(0) * rec_hi.unsqueeze(1)], dim=0)
+
+    rec_filters = rec_filters[:, None].repeat(out_size, 1, 1, 1)
+
+    return dec_filters, rec_filters
+
+def wavelet_transform(x, filters):
+    b, c, h, w = x.shape
+    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    x = F.conv2d(x, filters, stride=2, groups=c, padding=pad)
+    x = x.reshape(b, c, 4, h // 2, w // 2)
+    return x
+
+
+def inverse_wavelet_transform(x, filters):
+    b, c, _, h_half, w_half = x.shape
+    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    x = x.reshape(b, c * 4, h_half, w_half)
+    x = F.conv_transpose2d(x, filters, stride=2, groups=c, padding=pad)
+    return x
+
+class MBWTConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=5, stride=1, bias=True, wt_levels=1, wt_type='db1',ssm_ratio=1,forward_type="v05",):
+        super(MBWTConv2d, self).__init__()
+
+        assert in_channels == out_channels
+
+        self.in_channels = in_channels
+        self.wt_levels = wt_levels
+        self.stride = stride
+        self.dilation = 1
+
+        self.wt_filter, self.iwt_filter = create_wavelet_filter(wt_type, in_channels, in_channels, torch.float)
+        self.wt_filter = nn.Parameter(self.wt_filter, requires_grad=False)
+        self.iwt_filter = nn.Parameter(self.iwt_filter, requires_grad=False)
+
+        self.wt_function = partial(wavelet_transform, filters=self.wt_filter)
+        self.iwt_function = partial(inverse_wavelet_transform, filters=self.iwt_filter)
+
+        #self.global_atten =SS2D(d_model=in_channels, d_state=1,
+        #     ssm_ratio=ssm_ratio, initialize="v2", forward_type=forward_type, channel_first=True, k_group=2)
+        self.base_scale = _ScaleModule([1, in_channels, 1, 1])
+
+        self.wavelet_convs = nn.ModuleList(
+            [nn.Conv2d(in_channels * 4, in_channels * 4, kernel_size, padding='same', stride=1, dilation=1,
+                       groups=in_channels * 4, bias=False) for _ in range(self.wt_levels)]
+        )
+
+        self.wavelet_scale = nn.ModuleList(
+            [_ScaleModule([1, in_channels * 4, 1, 1], init_scale=0.1) for _ in range(self.wt_levels)]
+        )
+
+        if self.stride > 1:
+            self.stride_filter = nn.Parameter(torch.ones(in_channels, 1, 1, 1), requires_grad=False)
+            self.do_stride = lambda x_in: F.conv2d(x_in, self.stride_filter, bias=None, stride=self.stride,
+                                                   groups=in_channels)
+        else:
+            self.do_stride = None
+
+    def forward(self, x):
+
+        x_ll_in_levels = []
+        x_h_in_levels = []
+        shapes_in_levels = []
+
+        curr_x_ll = x
+
+        for i in range(self.wt_levels):
+            curr_shape = curr_x_ll.shape
+            shapes_in_levels.append(curr_shape)
+            if (curr_shape[2] % 2 > 0) or (curr_shape[3] % 2 > 0):
+                curr_pads = (0, curr_shape[3] % 2, 0, curr_shape[2] % 2)
+                curr_x_ll = F.pad(curr_x_ll, curr_pads)
+
+            curr_x = self.wt_function(curr_x_ll)
+            curr_x_ll = curr_x[:, :, 0, :, :]
+
+            shape_x = curr_x.shape
+            curr_x_tag = curr_x.reshape(shape_x[0], shape_x[1] * 4, shape_x[3], shape_x[4])
+            curr_x_tag = self.wavelet_scale[i](self.wavelet_convs[i](curr_x_tag))
+            curr_x_tag = curr_x_tag.reshape(shape_x)
+
+            x_ll_in_levels.append(curr_x_tag[:, :, 0, :, :])
+            x_h_in_levels.append(curr_x_tag[:, :, 1:4, :, :])
+
+        next_x_ll = 0
+
+        for i in range(self.wt_levels - 1, -1, -1):
+            curr_x_ll = x_ll_in_levels.pop()
+            curr_x_h = x_h_in_levels.pop()
+            curr_shape = shapes_in_levels.pop()
+
+            curr_x_ll = curr_x_ll + next_x_ll
+
+            curr_x = torch.cat([curr_x_ll.unsqueeze(2), curr_x_h], dim=2)
+            next_x_ll = self.iwt_function(curr_x)
+
+            next_x_ll = next_x_ll[:, :, :curr_shape[2], :curr_shape[3]]
+
+        x_tag = next_x_ll
+        assert len(x_ll_in_levels) == 0
+
+        #x = self.base_scale(self.global_atten(x))
+        #x = x + x_tag
+
+        if self.do_stride is not None:
+            x = self.do_stride(x)
+
+        return x
+
+
+class _ScaleModule(nn.Module):
+    def __init__(self, dims, init_scale=1.0, init_bias=0):
+        super(_ScaleModule, self).__init__()
+        self.dims = dims
+        self.weight = nn.Parameter(torch.ones(*dims) * init_scale)
+        self.bias = None
+
+    def forward(self, x):
+        return torch.mul(self.weight, x)
+
+class DWConv2d_BN_ReLU(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, bn_weight_init=1):
+        super().__init__()
+        self.add_module('dwconv3x3',
+                        nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=1, padding=kernel_size//2, groups=in_channels,
+                                  bias=False))
+        self.add_module('bn1', nn.BatchNorm2d(in_channels))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('dwconv1x1',
+                        nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, groups=in_channels,
+                                  bias=False))
+        self.add_module('bn2', nn.BatchNorm2d(out_channels))
+
+        # Initialize batch norm weights
+        nn.init.constant_(self.bn1.weight, bn_weight_init)
+        nn.init.constant_(self.bn1.bias, 0)
+        nn.init.constant_(self.bn2.weight, bn_weight_init)
+        nn.init.constant_(self.bn2.bias, 0)
+
+    @torch.no_grad()
+    def fuse(self):
+        # Fuse dwconv3x3 and bn1
+        dwconv3x3, bn1, relu, dwconv1x1, bn2 = self._modules.values()
+
+        w1 = bn1.weight / (bn1.running_var + bn1.eps) ** 0.5
+        w1 = dwconv3x3.weight * w1[:, None, None, None]
+        b1 = bn1.bias - bn1.running_mean * bn1.weight / (bn1.running_var + bn1.eps) ** 0.5
+
+        fused_dwconv3x3 = nn.Conv2d(w1.size(1) * dwconv3x3.groups, w1.size(0), w1.shape[2:], stride=dwconv3x3.stride,
+                                    padding=dwconv3x3.padding, dilation=dwconv3x3.dilation, groups=dwconv3x3.groups,
+                                    device=dwconv3x3.weight.device)
+        fused_dwconv3x3.weight.data.copy_(w1)
+        fused_dwconv3x3.bias.data.copy_(b1)
+
+        # Fuse dwconv1x1 and bn2
+        w2 = bn2.weight / (bn2.running_var + bn2.eps) ** 0.5
+        w2 = dwconv1x1.weight * w2[:, None, None, None]
+        b2 = bn2.bias - bn2.running_mean * bn2.weight / (bn2.running_var + bn2.eps) ** 0.5
+
+        fused_dwconv1x1 = nn.Conv2d(w2.size(1) * dwconv1x1.groups, w2.size(0), w2.shape[2:], stride=dwconv1x1.stride,
+                                    padding=dwconv1x1.padding, dilation=dwconv1x1.dilation, groups=dwconv1x1.groups,
+                                    device=dwconv1x1.weight.device)
+        fused_dwconv1x1.weight.data.copy_(w2)
+        fused_dwconv1x1.bias.data.copy_(b2)
+
+        # Create a new sequential model with fused layers
+        fused_model = nn.Sequential(fused_dwconv3x3, relu, fused_dwconv1x1)
+        return fused_model
+
+
+class I2Block(nn.Module):
+    def __init__(
+        self, conv, n_feat, kernel_size,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1,head_num=1,win_num_sqrt=16,window_size=16):
+        super(I2Block, self).__init__()
+        self.wave=MBWTConv2d(64,64,3, wt_levels=1, ssm_ratio=1, forward_type="v052d",)
+        self.dwconv=DWConv2d_BN_ReLU(64,64,3)
+        self.res_scale = res_scale
+        self.unet=UNet(64,64)
+        '''
+        self.mamba= Mamba(
+    # This module uses roughly 3 * expand * d_model^2 parameters
+    d_model=256, # Model dimension d_model
+    d_state=16,  # SSM state expansion factor
+    d_conv=4,    # Local convolution width
+    expand=2,    # Block expansion factor
+    )
+'''
+    def forward(self, x):
+        
+        x_dw=self.dwconv(x)
+        x_wave=self.wave(x)
+        #x_u=self.unet(x)
+        #mamba_x=einops.rearrange(x,'b d h w -> (b d) h w')
+        #mamba_x=mamba_x.contiguous()
+        #output = self.mamba(mamba_x)
+        #x_mamba=einops.rearrange(output,'(b d) h w -> b d h w',b=x.shape[0])
+        
+        #out = x_dw + x_wave + x_mamba + x
+        out = x_dw + x_wave  + x
+        #out=x_u+x
+        return out
+
+class I2Group(nn.Module):
+    def __init__(
+        self, conv, n_depth, n_feat, kernel_size,skip_connect=False,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1,head_num=1,win_num_sqrt=16,window_size=16):
+        super().__init__()
+
+        body = [I2Block(conv, n_feat, kernel_size,
+                            bias, bn, act, res_scale,head_num,win_num_sqrt) for _ in range(n_depth)]
+
+        self.body = nn.Sequential(*body)
+    def forward(self,x):
+        x_f = self.body(x)
+        out = x_f
+        return out
+
+
+class CrossViewBlock(nn.Module):
+    def __init__(self,n_feat):
+        super().__init__()
+
+        self.norm = nn.LayerNorm(n_feat)
+        #self.transformer=TransformerBlock(64,8,256)
+        self.conv_sag = nn.Sequential(
+            nn.Conv2d(n_feat,n_feat,1,1,0),
+            Rearrange('b c h w -> b h c w'),
+            nn.PixelShuffle(2),
+            nn.Conv2d(64,64,3,1,1),
+            nn.ReLU(),
+            nn.Conv2d(64,64,3,1,1),
+            nn.PixelUnshuffle(2),
+            Rearrange('b h c w -> b c h w'),
+        )
+        
+        self.conv_cor = nn.Sequential(
+                nn.Conv2d(n_feat,n_feat,1,1,0), #b 64 256 256
+                Rearrange('b c h w -> b w c h'),    #b 256 64 256
+            nn.PixelShuffle(2), #b 64 128 512
+            nn.Conv2d(64,64,3,1,1),
+            nn.ReLU(),
+            nn.Conv2d(64,64,3,1,1),
+            nn.PixelUnshuffle(2),
+            Rearrange('b w c h -> b c h w'),
+        )
+
+    def forward(self,x):
+        B,C,H,W = x.shape
+        x = einops.rearrange(x,'b c h w -> b (h w) c')
+        x = self.norm(x)
+        x = einops.rearrange(x,'b (h w) c -> b c h w',h=H,w=W)
+
+        x_sag_f = self.conv_sag(x) # b c h w
+        x_cor_f = self.conv_cor(x) # b c h w
+        x_out = x_cor_f + x_sag_f
+        return x_out
+    
+
+class newmodel(nn.Module):
+    def __init__(self,args=None,conv=default_conv):
+        super(newmodel, self).__init__()
+        self.args = args
+        blocks=1
+        cblayers=2
+        depth=1
+        '''
+        blocks=1
+        cblayers=3
+        depth=2
+        '''
+        n_feats = args.n_feats #64
+        kernel_size = args.kernel_size # 3
+        num_blocks = args.num_blocks # 16
+        act = nn.ReLU(True)
+        res_scale = args.res_scale # 1
+        in_slice = args.lr_slice_patch*1
+        out_slice = args.hr_slice_patch
+
+        head_num = args.head_num
+        win_num_sqrt = args.win_num_sqrt
+        window_size = args.window_size
+        self.head = nn.Sequential(conv(in_slice,n_feats,kernel_size),
+                                  nn.ReLU(),
+                                  conv(n_feats,n_feats,kernel_size))
+        modules_body = [
+            I2Group(
+                conv, n_depth=depth,n_feat=n_feats, kernel_size=kernel_size, act=act, res_scale=res_scale, 
+                head_num=head_num, win_num_sqrt=win_num_sqrt,window_size=window_size) for _ in range(blocks)]
+        self.body = nn.ModuleList(modules_body)
+        
+        self.alignment = nn.ModuleList([CrossViewBlock(n_feats) for _ in range(cblayers)])
+
+        self.fuse_align = nn.Conv2d(cblayers*n_feats,n_feats,1,1,0)
+
+        modules_tail = [
+            conv(n_feats, n_feats, kernel_size),
+            nn.ReLU(),
+            conv(n_feats,out_slice,kernel_size)]
+        self.tail = nn.Sequential(*modules_tail)
+        self.aux_s = nn.Sequential(
+                nn.Conv2d(64, 32, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
+                nn.Dropout2d(p=0.1),
+                nn.Conv2d(32, out_slice, kernel_size=1)
+            )
+        
+    def forward(self, x):
+        #print('model_name=mamba_unet')
+        x = x.permute(0,3,1,2)
+        x = x.contiguous()
+        x_head = self.head(x) 
+        
+        res = x_head
+
+        align_list = []
+        res = self.alignment[0](res)+res
+        align_list.append(res)
+        id_list=[]
+        #id_list=[0]
+
+        id_list=[0,1]
+        for id,layer in enumerate(self.body):
+            res = layer(res)
+            if  id==0:
+                res_middle=self.aux_s(res)
+            if id in id_list:
+                res = self.alignment[id//2+1](res) + res
+                align_list.append(res)
+
+        res = self.fuse_align(torch.cat(align_list,1))
+        
+        res += x_head       
+        
+        out = self.tail(res) # [bz,s,h,w]
+        
+        out[:,::self.args.upscale] = x
+        out = out.permute(0,2,3,1).contiguous()
+        
+        return out,res_middle
+        #return out
+
+if __name__ == '__main__':
+    import argparse
+    args = argparse.Namespace()
+    args.upscale = 2
+    args.n_feats = 64
+    args.kernel_size = 3
+    args.res_scale = 1
+    args.num_blocks = 16
+    args.lr_slice_patch = 4
+    args.hr_slice_patch = (args.lr_slice_patch-1)*args.upscale + 1
+    args.head_num = 1
+    args.win_num_sqrt = 16
+    args.window_size = 16
+    args.n_size = 256
+
+    gpy_id = 0
+    model = newmodel(args).cuda(gpy_id)
+    #x = torch.ones(1,args.n_size,args.n_size,args.lr_slice_patch).cuda(gpy_id)
+    #y = torch.ones(1,args.n_size,args.n_size,args.hr_slice_patch).cuda(gpy_id)
+    x=torch.randn(1,256,256,4).cuda(gpy_id)
+    pred=model(x)
+    print(pred.shape)
+    
+    #from torchsummary import summary
+    #summary(model,(256, 256, 4))
+    
+    #from thop import profile
+    #flops,params=profile(model,(x,))
+    #print('flops: %.2f M, params: %.2f M' % (flops / 1000000.0, params / 1000000.0))
